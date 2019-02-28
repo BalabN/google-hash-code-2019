499e743ead476f60e79c177bcdfefa052df48297
Tadej Bukovec <tadej.bukovec@outfit7.com>
2019-02-28 19:40:43 +0100

    Added logic for finding minimum number of matching tags for each photo

--- src/main/java/models/Photo.java
+++ src/main/java/models/Photo.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class Photo {
@@ -40,5 +41,38 @@ public class Photo {
 
     public boolean addTag(String tag) {
         return this.tags.add(tag);
+    }
+
+    public int CountMinimumCommonFactor(Photo photo, ArrayList<Photo> inputCollection){
+
+        ArrayList<Photo> photoCollection = inputCollection;
+        //remove the photo from the collection - we dont want it to compare to itself;
+        photoCollection.remove(photo);
+
+        int minimumCommonTagCount = 0;
+        for (Photo collectionPhoto : photoCollection) {
+            if (minimumCommonTagCount < CountCommonTags(photo, collectionPhoto)){
+                minimumCommonTagCount = CountCommonTags(photo, collectionPhoto);
+            }
+        }
+        return minimumCommonTagCount;
+    }
+
+    private int CountCommonTags(Photo photo1, Photo photo2){
+
+        int currentMaximumCommonTagCount = 0;
+
+        for (String tag1: photo1.getTags()) {
+            int matches = 0;
+            for (String tag2 : photo2.getTags()){
+                if (tag1 == tag2){
+                    matches += 1;
+                }
+            }
+            if (currentMaximumCommonTagCount < matches) {
+                currentMaximumCommonTagCount = matches;
+            }
+        }
+        return currentMaximumCommonTagCount;
     }
 }

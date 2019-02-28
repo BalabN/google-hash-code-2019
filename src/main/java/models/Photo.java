0e1190798143ed272b57d304aac9cd5b04ae89b9
jumPEK <jernej.koderman@gmail.com>
2019-02-28 19:09:19 +0100

    added constructor

--- src/main/java/models/Photo.java
+++ src/main/java/models/Photo.java
@@ -1,4 +1,34 @@
 package models;
 
+import java.util.List;
+
 public class Photo {
+
+    private int id;
+    private int orientation;
+    private List tags;
+
+    public int getOrientation(){
+        return orientation;
+    }
+
+    public int id(){
+        return id;
+    }
+
+    public List tags (){
+        return tags;
+    }
+
+    public void setTags(List tags){
+        this.tags = tags;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setOrientation(int orientation){
+        this.orientation = orientation;
+    }
 }
